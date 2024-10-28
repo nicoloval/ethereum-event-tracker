d60eef9eedb3fa9eeb898d3881872bc1f5748e47
Nicolo Vallarano <nicolo.vallarano@business.uzh.ch>
2024-10-28 12:36:50 +0000

    added logger

--- event_tracker.py
+++ event_tracker.py
@@ -2,4 +2,14 @@
 find all the occurences of a specific event.
 """
 from config import *
-from hello import hello
+from web3 import Web3
+import logger, logging
+
+# global vars
+REQ_SIZE=10000
+
+# establish w3 conn
+w3_pow = Web3(Web3.HTTPProvider(os.getenv('POW_RPC_ENDPOINT')))
+w3_pos = Web3(Web3.HTTPProvider(os.getenv('POS_RPC_ENDPOINT')))
+print(f'PoW chain connected?: {w3_pow.is_connected()}')
+print(f'PoS chain connected?: {w3_pos.is_connected()}')
---
+++ logger.py
@@ -0,0 +1,49 @@
+import logging
+import os, sys
+from datetime import datetime
+class StreamToLogger:
+    """
+    Fake file-like stream object that redirects writes to a logger instance, while also printing to console.
+    """
+    def __init__(self, logger, log_level=logging.INFO, stream=None):
+        self.logger = logger
+        self.log_level = log_level
+        self.stream = stream  # Original stream (stdout or stderr)
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            # More accurate checking for JSON-like structures
+            if line.strip().startswith('[{"') and line.strip().endswith('}]'):
+                continue  # Skip logging this line if it matches the pattern
+            self.logger.log(self.log_level, line.rstrip())
+            if self.stream:
+                self.stream.write(line + '\n')  # Append newline for console output
+                self.stream.flush()
+
+    def flush(self):
+        pass
+
+def setup_logging():
+    os.makedirs('./logs', exist_ok=True)
+    current_time = datetime.now().strftime("%Y-%m-%d")
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+        filename=f'./logs/job_{current_time}.log',
+        filemode='a'
+    )
+    # Capture original stdout and stderr
+    original_stdout = sys.stdout
+    original_stderr = sys.stderr
+    # Redirect stdout and stderr to logging and also print to original streams
+    sys.stdout = StreamToLogger(logging.getLogger('STDOUT'), logging.INFO, original_stdout)
+    sys.stderr = StreamToLogger(logging.getLogger('STDERR'), logging.ERROR, original_stderr)
+
+if __name__ == "__main__":
+    setup_logging()
+    print("This message will be logged and also displayed on the console.")
+    print('[{"name": "Example", "type": "test"}]')
+    try:
+        raise ValueError("This error will be logged and displayed.")
+    except Exception as e:
+        print(e)
