d19b7ad5dbc9d7a7dac4bb7fbd22b2f5eeaf11c0
Nicolo Vallarano <nicolo.vallarano@business.uzh.ch>
2024-10-28 12:13:31 +0000

    added contract addresses in config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 TRX_FOLDER="/local/scratch/diamond_coin/token_txs/token_txs_0_19250000/"
+
+stETH_address = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
+wstETH_address = "    0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
+LDO_address = "    0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32"
