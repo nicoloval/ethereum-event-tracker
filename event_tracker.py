cf18e05cff734fa9a43ff73796096b3a51757b3f
Nicolo Vallarano <nicolo.vallarano@business.uzh.ch>
2024-10-29 09:22:24 +0000

    managing the output folder

--- event_tracker.py
+++ event_tracker.py
@@ -43,19 +43,10 @@ def generate_hash_nonce():
     nonce = hash_object.hexdigest()
     return nonce[:64]  # Truncate to fit 32 bytes since Solidity bytes32 type is 32 bytes long
 
-# DEBUG: this Transfer happened
-# blockNumber                                         18006108
-# from              0xadce54380e5b5f62eab82a34ce27f904e4172af3
-# to                0x889edc2edab5f40e902b864ad4d7ade8e412f9b1
-# fromIsContract                                             0
-# toIsContract                                               1
-# value                                     420004762656680918
-# listen to event from block fromblock
-fromblock = 18006105 #TODO: set
-# to recent block
+
+# normal setting
+recent_block = w3.eth.block_number
 # last block on abacus-1: 18012051
-# recent_block = w3.eth.block_number
-recent_block = 18006110
 
 # prep log decoding 
 contract_abi_path =  './contracts/stETH-abi.json'
@@ -64,9 +55,31 @@ contract_abi = json.load(f)
 contract = w3.eth.contract(address=os.getenv('stETH_address'), abi=contract_abi)
 event_abi_map = log_decoder.generate_event_abi_map(contract_abi)
 
+# initialize output csv
+output_path = './output/stETH_Transfer.csv' 
+if not os.path.exists(f'{history_path}'):
+    os.makedirs('./output', exist_ok=True)
+    logger.info(f'no previous output file exists')
+    fromblock = 11000000
+    history=pd.DataFrame()
+else:
+    logger.info(f'output file exists')
+    history = pd.read_csv(f'{history_path}')
+    fromblock = history.iloc[-1]['blockNumber']+1
+
 # Initialize an empty DataFrame to store the logs
 output = pd.DataFrame(columns=['blockNumber', 'from', 'to', 'value'])
 
+# DEBUG: this Transfer happened
+# blockNumber                                         18006108
+# from              0xadce54380e5b5f62eab82a34ce27f904e4172af3
+# to                0x889edc2edab5f40e902b864ad4d7ade8e412f9b1
+# fromIsContract                                             0
+# toIsContract                                               1
+# value  
+# # setting for the debug                                   420004762656680918
+# fromblock = 18006105
+# recent_block = 18006110
 for ii, step in enumerate(np.arange(fromblock, recent_block, REQ_SIZE)):
 
     toblock = min(step + REQ_SIZE, recent_block)
@@ -102,6 +115,6 @@ for ii, step in enumerate(np.arange(fromblock, recent_block, REQ_SIZE)):
             output = pd.concat([output, success_row], ignore_index=True)
 
     # Save the DataFrame to a CSV file
-    output.to_csv('./output/stETH_Transfer.csv', index=False, mode='a', header=not os.path.exists('./output/stETH_Transfer.csv'))
+    output.to_csv(output_path, index=False, mode='a', header=not os.path.exists(output_path))
 
 logger.info(f'Event Tracker job finished.')
