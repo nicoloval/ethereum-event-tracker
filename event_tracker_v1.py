51195494d26c4fc582eb1a41bc90a40a86b88e59
Nicolo Vallarano <nicolo.vallarano@business.uzh.ch>
2024-10-29 13:34:35 +0000

    generalized event tracker v1 to track every possible event

--- event_tracker_v1.py
+++ event_tracker_v1.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 import pandas as pd
 import json
 import numpy as np
+from parse_solidity_event import parse_solidity_event
+import sys
 
 logger.setup_logging()
 logger = logging.getLogger()
@@ -18,22 +20,44 @@ logger = logging.getLogger()
 load_dotenv('.env')
 
 class EventTrackerConfig:
-    def __init__(self, contract_abi_path, contract, event_solidity_path, from_block, recent_block):
-        self.contract_abi_path = contract_abi_path
-        self.contract = contract
-        self.event_solidity_path = event_solidity_path
+    def __init__(self, contract_name, contract_address, event_name, from_block, recent_block, append):
+        self.contract_name = contract_name
+        self.contract_address = contract_address
+        self.event_name = event_name
         self.from_block = from_block
         self.recent_block = recent_block
+        self.append = append
+        # Configure the paths to the files form user information
+        self.contract_abi_path = f"./abi/{self.contract_name}.json"
+        self.event_solidity_path = f"./event/{self.contract_name}-{self.event_name}.sol"
+        self.output_path = f"./output/{self.contract_name}-{self.event_name}.csv"
+
+
+import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Event Tracker Configuration")
-    parser.add_argument('--contract_abi_path', type=str, required=True, help='Path to the contract ABI')
-    parser.add_argument('--contract', type=str, required=True, help='Contract address')
-    parser.add_argument('--event_solidity_path', type=str, required=True, help='Path to the event solidity file')
-    parser.add_argument('--from_block', type=int, default=None, help='Starting block number (optional)')
-    parser.add_argument('--recent_block', type=int, default=None, help='Stopping block number (optional)')
+    parser.add_argument('-n', '--contract-name', type=str, required=True, help='Contract name')
+    parser.add_argument('-a', '--contract-address', type=str, required=True, help='Contract address')
+    parser.add_argument('-e', '--event-name', type=str, required=True, help='Event name')
+    parser.add_argument('-f', '--from-block', type=int, default=None, help='Starting block number (optional)')
+    parser.add_argument('-r', '--recent-block', type=int, default=None, help='Stopping block number (optional)')
+    parser.add_argument('-p', '--append', action="store_true", help='Append to existing output (optional)')
+
     args = parser.parse_args()
-    return EventTrackerConfig(args.contract_abi_path, args.contract, args.event_solidity_path, args.from_block, args.recent_block)
+    
+    # Assuming EventTrackerConfig is a class you've defined elsewhere
+    return EventTrackerConfig(
+        args.contract_name,
+        args.contract_address,
+        args.event_name,
+        args.from_block,
+        args.recent_block,
+        args.append  # Passes the boolean as required
+    )
+
+
+# TK opptimally set
 REQ_SIZE=10000
 
 # Parse arguments
@@ -53,32 +77,43 @@ def get_logs_try(w3,filter_params):
     logs = w3.eth.get_logs(filter_params)
     return logs
 
-
-# normal setting
-recent_block = config.recent_block
 # last block on abacus-1: 18012051
+# set blocks range to examine
+if config.from_block is None:
+    fromblock = 0
+else:
+    fromblock = config.from_block
+
+if config.recent_block is None:
+    recent_block = w3.eth.block_number
+else:
+    recent_block = config.recent_block
 
 # prep log decoding 
-contract_abi_path = config.contract_abi_path
-f = open(contract_abi_path)
+f = open(config.contract_abi_path)
 contract_abi = json.load(f)
-contract = w3.eth.contract(address=config.contract, abi=contract_abi)
+contract = w3.eth.contract(address=config.contract_address, abi=contract_abi)
 event_abi_map = log_decoder.generate_event_abi_map(contract_abi)
 
-# initialize output csv
-output_path = './output/stETH_Transfer.csv' 
-if not os.path.exists(f'{output_path}'):
-    os.makedirs('./output', exist_ok=True)
-    logger.info(f'no previous output file exists')
-    fromblock = config.from_block
-    history=pd.DataFrame()
-else:
-    logger.info(f'output file exists')
-    history = pd.read_csv(f'{history_path}')
-    fromblock = history.iloc[-1]['blockNumber']+1
+# event .sol information parsing
+event = parse_solidity_event(config.event_solidity_path)
 
-# Initialize an empty DataFrame to store the logs
-output = pd.DataFrame(columns=['blockNumber', 'from', 'to', 'value'])
+# Initialize an empty DataFrame to store the event occurences
+output = pd.DataFrame(columns=['blockNumber'] + event['fields'])
+
+# initialize output csv
+if not os.path.exists(f'{config.output_path}'):
+    os.makedirs('./output', exist_ok=True)
+    logger.info(f'No previous output file exists')
+else:
+    if config.append:
+        logger.info(f'Appending to previous existing output')   
+        history = pd.read_csv(f'{config.output_path}')
+        fromblock = history.iloc[-1]['blockNumber']+1        
+    else:
+        output.to_csv(config.output_path, index=False, mode='w', header=True)
+        logger.info(f'Overwriting previous exisiting output')
+
 
 # DEBUG: this Transfer happened
 # blockNumber                                         18006108
@@ -90,6 +125,7 @@ output = pd.DataFrame(columns=['blockNumber', 'from', 'to', 'value'])
 # # setting for the debug                                   420004762656680918
 # fromblock = 18006105
 # recent_block = 18006110
+
 for ii, step in enumerate(np.arange(fromblock, recent_block, REQ_SIZE)):
 
     toblock = min(step + REQ_SIZE, recent_block)
@@ -98,10 +134,11 @@ for ii, step in enumerate(np.arange(fromblock, recent_block, REQ_SIZE)):
     args['fromBlock'] = step 
     args['toBlock'] = toblock 
     # the contract address
-    args['address'] = stETH_address
-    # the Transfer event to track
+    args['address'] = config.contract_address
+    # the event to track
+    str2kek = event['event_name'] + '(' + ",".join(event['types']) + ')'
     # the 0x depends on the web3 version
-    args['topics'] = ['0x'+ Web3.keccak(text='Transfer(address,address,uint256)').hex()]
+    args['topics'] = ['0x'+ Web3.keccak(text=str2kek).hex()]
     filter_params = log_filters.make_filter(args)
 
     # fetching logs
@@ -116,15 +153,14 @@ for ii, step in enumerate(np.arange(fromblock, recent_block, REQ_SIZE)):
             # generate nonce as an identifier
             work={}
             work['blockNumber']=decoded_log['blockNumber']
-            work['from']=decoded_log['args']['from']
-            work['to']=decoded_log['args']['to']
-            work['value'] = decoded_log['args']['value']  # in wei
+            for field in event['fields']:
+                work[field] = decoded_log['args'][field]
 
             # Append the work dictionary as a new row to the DataFrame
             success_row = pd.DataFrame([work])
             output = pd.concat([output, success_row], ignore_index=True)
 
     # Save the DataFrame to a CSV file
-    output.to_csv(output_path, index=False, mode='a', header=not os.path.exists(output_path))
+    output.to_csv(config.output_path, index=False, mode='a', header=not os.path.exists(config.output_path))
 
 logger.info(f'Event Tracker job finished.')
--- readme.md
+++ readme.md
@@ -1,3 +1,24 @@
-- abi json file, in folder `/contracts`. E.g. `stETH-abi.json`
-- contract address. E.g. `stETH_address="0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"`
-- event to track in .sol format, in folder `/events`
+First, you need to save the following files in the correct directory:
+- the abi json file of the contract goes in the folder `./abi/`, the file name is the contract name. 
+    e.g.
+    ```
+    ./abi/stETH.json
+    ```
+- the event in solidity format goes into the `./event/` folder, the name is the contract name, dash, event name.
+    e.g.
+    ```
+    ./event/stETH-Transer.sol
+    ```
+
+You can now call the script, using the following command:
+```
+python event_tracker.py -contract-name stETH --contract-address 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84 --event-name Transfer
+```
+- contract address is the address of the contract. in the example is stETH token contract address.
+- additional arguments can be passed to limit the range of blocks to search. Default behaviour is from block 0 to last block in the node.
+    ```
+    python event_tracker.py --contract-name stETH --contract-address 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84 --event-name Transfer --from-block 18006105 --recent-block 18006110
+    ```
+- an additional boolean option can be set to firce the wscript to append to an existing output file. The script will automatically start form the last block recorded on the output.
+
+The output is saved in the folder `./output`, in a csv following the naming convention `./output/{contract_name}-{event_name}.csv`.
