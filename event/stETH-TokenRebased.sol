0d183ea886a106ff0079c735ac44db2e44206bfd
nicoloval <nicolo.vallarano@imtlucca.it>
2024-11-01 16:04:21 +0000

    added TokenRebased event

---
+++ event/stETH-TokenRebased.sol
@@ -0,0 +1,9 @@
+emit TokenRebased(
+_reportedData.reportTimestamp,
+_reportedData.timeElapsed,
+_reportContext.preTotalShares,
+_reportContext.preTotalPooledEther,
+postTotalShares,
+postTotalPooledEther,
+_reportContext.sharesMintedAsFees
+);
