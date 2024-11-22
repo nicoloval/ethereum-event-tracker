b12d3e6a42982615c7e9a76260e52533637750cc
nicoloval <nicolo.vallarano@imtlucca.it>
2024-11-22 15:27:50 +0000

    removed tests

--- tests/test_process_event_tracker.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import pandas as pd
-from unittest.mock import patch, MagicMock
-from process_event_tracker import EventTrackerConfig, get_logs_try
-from web3 import Web3
-
-if __name__ == '__main__':
-    unittest.main()
